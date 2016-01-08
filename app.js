032095372a7d3abae8a69f66fda91fd1d0439559
AlbertoOlla <webservicesolla@gmail.com>
2016-01-08 12:52:01 +0100

    Created Main Controller and display fake posts

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var app= angular.module('flapperNews',[]);
 
 app.controller('MainCtrl',['$scope',function($scope){
-	$scope.test = 'Hello World!';
+
+	$scope.posts = [
+	  {title: 'post 1', upvotes: 5},
+	  {title: 'post 2', upvotes: 2},
+	  {title: 'post 3', upvotes: 15},
+	  {title: 'post 4', upvotes: 9},
+	  {title: 'post 5', upvotes: 4}
+	];
 }]);
