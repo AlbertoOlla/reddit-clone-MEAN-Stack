01e2a044fa9822d011cbc4fbf64d5c53f3115a58
AlbertoOlla <webservicesolla@gmail.com>
2016-01-08 13:11:49 +0100

    Added user input

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ var app= angular.module('flapperNews',[]);
 
 app.controller('MainCtrl',['$scope',function($scope){
 
+	$scope.addPost = function(){
+
+		if(!$scope.title || $scope.title === '' ){ return; }
+
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes:0
+		});
+		
+		$scope.title = '';
+		$scope.link = '';		
+	};
+
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	}
+
+
 	$scope.posts = [
 	  {title: 'post 1', upvotes: 5},
 	  {title: 'post 2', upvotes: 2},
