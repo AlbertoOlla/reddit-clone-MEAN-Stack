5392d783f2f2157a5d6bc5f8dcb8f340e1fb8d32
Alberto Olla <webservicesolla@gmail.com>
2016-01-11 18:34:35 +0100

    Adding Express Server, Mongoose Schemas and REST Routes

--- app.js
+++ app.js
@@ -1,89 +1,65 @@
-var app = angular.module('flapperNews',['ui.router']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+require('./models/Post');
+require('./models/Comment');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+mongoose.connect('mongodb://localhost/news');
 
+var app = express();
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: './home.html',
-      controller: 'MainCtrl'
-    })
-    .state('posts', {
-      url: '/posts/{id}',
-      templateUrl: './posts.html',
-      controller: 'PostsCtrl'
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
+}
 
-  $urlRouterProvider.otherwise('home');
-}]);
-
-
-app.factory('posts',[function(){
-	var o = {
-		posts : [
-	  {title: 'post 1', upvotes: 5, comments: []},
-	  {title: 'post 2', upvotes: 2, comments: []},
-	  {title: 'post 3', upvotes: 15, comments: []},
-	  {title: 'post 4', upvotes: 9, comments: []},
-	  {title: 'post 5', upvotes: 4, comments: []}
-	]
-	};
-
-	return o;
-}]);
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
-app.controller('PostsCtrl',['$scope','$stateParams','posts',
-	function($scope, $stateParams, posts){
-
-		$scope.post = posts.posts[$stateParams.id];
-
-		$scope.addComment = function(){
-
-			if( !$scope.body || $scope.body === '' ){ return; }
-
-			$scope.post.comments.push({
-				author: 'test',
-				body: $scope.body,
-				upvotes: 0
-			});
-
-			$scope.body = '';
-		};
-
-}]);
-
-app.controller('MainCtrl',['$scope','posts',function($scope,posts){
-
-	$scope.posts = posts.posts;
-
-	$scope.addPost = function(){
-
-		if(!$scope.title || $scope.title === '' ){ return; }
-
-		$scope.posts.push({
-			title: $scope.title,
-			link: $scope.link,
-			upvotes:0,
-			comments: [
-				{author: 'Joe', body: 'Cool post!', upvotes: 0},
-				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-			]
-		});
-
-		$scope.title = '';
-		$scope.link = '';		
-	};
-
-	$scope.incrementUpvotes = function(post){
-		post.upvotes += 1;
-	}
-
-}]);
-
-
+module.exports = app;
