b1d1da5e05d0142b80560f68aafff390215360f8
Alberto Olla <webservicesolla@gmail.com>
2016-01-11 11:40:09 +0100

    Added Angular routing

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-var app= angular.module('flapperNews',[]);
+var app = angular.module('flapperNews',['ui.router']);
+
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: './home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+/*
+app.run(($rootScope) => {
+  $rootScope.$on("$stateChangeError", console.log.bind(console));
+});
+*/
+
+app.factory('posts',[function(){
+	var o = {
+		posts : [
+	  {title: 'post 1', upvotes: 5},
+	  {title: 'post 2', upvotes: 2},
+	  {title: 'post 3', upvotes: 15},
+	  {title: 'post 4', upvotes: 9},
+	  {title: 'post 5', upvotes: 4}
+	]
+	};
+
+	return o;
+}]);
+
 
 app.controller('MainCtrl',['$scope','posts',function($scope,posts){
 
@@ -24,16 +62,4 @@ app.controller('MainCtrl',['$scope','posts',function($scope,posts){
 
 }]);
 
-app.factory('posts',[function(){
-	var o = {
-		posts : [
-	  {title: 'post 1', upvotes: 5},
-	  {title: 'post 2', upvotes: 2},
-	  {title: 'post 3', upvotes: 15},
-	  {title: 'post 4', upvotes: 9},
-	  {title: 'post 5', upvotes: 4}
-	]
-	};
 
-	return o;
-}]);
